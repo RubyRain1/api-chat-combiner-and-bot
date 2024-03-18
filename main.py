b9652211c9c258b54308ff22a1ebac37cfa7194b
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2024-03-18 10:09:20 -0400

    main set up
    this is a re set up of my previous work. This is being remade for privacy reasons

---
+++ main.py
@@ -0,0 +1,36 @@
+from twitchio.ext import commands 
+import config
+
+
+val = input("Enter your channel name: ")
+val2 = input("Enter second channel's name: ")
+
+class Bot(commands.Bot):
+    
+    
+    
+    def __init__(self):
+
+        super().__init__(token=config.api_key, prefix='?', initial_channels=[val,val2])
+        
+    async def event_message(self, messagae):
+
+        print(f'Logged in as | {self.nick}')
+        print(f'user id is | {self.user_id}')
+
+    async def event_message(self, message):
+
+        if message.echo:
+            return
+        
+        print(message.content)
+
+        await self.handle_commands(message)
+
+    @commands.command()
+    async def hello(self, ctx: commands.Context):
+        await ctx.send(f'Hello {ctx.author.name}')
+
+
+bot = Bot()
+bot.run()

3ce9482048dd1001a88d43f39704f9bcf698d596
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2024-04-01 13:07:24 -0400

    commands and sectioning
    I took time to section out the main.py file in order to just be more clean with things, eventually this might be separated into separate files as well but for now kept simple.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from twitchio.ext import commands 
 import config
+import random
 
-    
+#twitch username input
 val = input("Enter your channel name: ")
 val2 = input("Enter second channel's name: ")
 
@@ -11,6 +12,7 @@ class Bot(commands.Bot):
     
     def __init__(self):
 
+        #chat connection
         super().__init__(token=config.api_key, prefix='?', initial_channels=[val,val2])
         
     async def event_message(self, messagae):
@@ -18,6 +20,7 @@ class Bot(commands.Bot):
         print(f'Logged in as | {self.nick}')
         print(f'user id is | {self.user_id}')
 
+    #chat combiner code
     async def event_message(self, message):
 
         if message.echo:
@@ -30,13 +33,30 @@ class Bot(commands.Bot):
 
         await self.handle_commands(message)
 
+    # hello command
     @commands.command()
     async def hello(self, ctx: commands.Context):
         await ctx.send(f'Hello {ctx.author.name}')
 
+    #cookie command
     @commands.command()
     async def cookie(self, ctx: commands.Context):
         await ctx.send(f'{ctx.author.name} gets a cookie!!!')
 
+    #8ball command
+        
+    #milk gamble command
+    
+    @commands.command()
+    async def milk(self, ctx: commands.Context):
+        milk = round(random.uniform(99,100), 2)
+        if (milk >= 99):
+           mMilk = "you get the all fated choccy milk"
+           await ctx.send(f'{ctx.author.name} {mMilk}')
+        else:
+            await ctx.send(f'you lose {milk}')
+            
+    #black jack command?
+    
 bot = Bot()
 bot.run()

0c1926be8897992a1ce44fb555b1a4e5f34ecdb1
Holden Hughes <100702336+RubyRain1@users.noreply.github.com>
2024-04-01 11:02:47 -0400

    reverting code for test
    this is to test if the exe will have to be generated always from the old twitch-api hidden repo(same code) or if I can actually generate it from this file.

--- main.py
+++ main.py
@@ -10,17 +10,19 @@ if TYPE_CHECKING:
     
 val = input("Enter your channel name: ")
 val2 = input("Enter second channel's name: ")
-       
-            
-
 
 class Bot(commands.Bot):
+    
+    
     
     def __init__(self):
 
         super().__init__(token=config.api_key, prefix='?', initial_channels=[val,val2])
         
-   
+    async def event_message(self, messagae):
+
+        print(f'Logged in as | {self.nick}')
+        print(f'user id is | {self.user_id}')
 
     async def event_message(self, message):
 
@@ -28,20 +30,13 @@ class Bot(commands.Bot):
             return
         
         print(f'{message.author.display_name}: {message.content}')
-        
-       
-        
 
         await self.handle_commands(message)
 
-    
     @commands.command()
     async def hello(self, ctx: commands.Context):
         await ctx.send(f'Hello {ctx.author.name}')
 
 
-
 bot = Bot()
 bot.run()
-
-

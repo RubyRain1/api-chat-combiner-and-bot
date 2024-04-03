e6ffd4e5de03ea19751478932049d72f19fe00c7
HurricaneHolden <100702336+RubyRain1@users.noreply.github.com>
2024-04-03 13:16:20 -0400

    two new commands
    this adds a random generator for responses to two commands

--- main.py
+++ main.py
@@ -43,29 +43,37 @@ class Bot(commands.Bot):
         await ctx.send(f'{ctx.author.name} gets a cookie!!!')
 
     #8ball command
-        
-    #milk gamble command
+    @commands.command()
+    async def fortune(self, ctx: commands.Context):
+        fortune = random.randint(1,100) #this gets a random number 1-100 in order for logic to run.
+        if fortune <= 25:
+            await ctx.send(f'{ctx.author.name} yes I assume so.')
+        elif fortune <= 50:
+            await ctx.send(f'{ctx.author.name} no this is unlikely.')
+        elif fortune <= 75: 
+            await ctx.send(f'{ctx.author.name} there is but a chance.')
+        elif fortune <= 100: 
+            await ctx.send(f'{ctx.author.name} perchance :skull:')
     
+
+    #milk gamble command
     @commands.command()
     async def milk(self, ctx: commands.Context):
         #this is going to be the random logic for the command.
-
-        milk = round(random.uniform(1,100), 2) 
-        if(milk <= 33.33): 
-            mMilk = "you get the all fated choccy milk"
+        milk = round(random.uniform(1,100), 2) #this gets a random float number from 1-100 and stops it at 2 dec points.
+        if milk <= 33.33:
+             mMilk = "you regular milk :3"
+             await ctx.send(f'{ctx.author.name} {mMilk}')
+        elif milk <= 66.66:
+             mMilk = "you get the kinda mid strawberry milk if we are really being honest here."
+             await ctx.send(f'{ctx.author.name} {mMilk}')
+        elif milk <= 99.99:
+            mMilk = "you get the all fated choccy milk."
             await ctx.send(f'{ctx.author.name} {mMilk}')
-            
-      #  match milk:
-           # case range(0,90):
-             #   mMilk = "you get strawberry milk"
-               # await ctx.send(f'{ctx.author.name} {mMilk}')
-
-           # case 100: 
-              #  mMilk = "you get strawberry milk"
-               # await ctx.send(f'{ctx.author.name} {mMilk}')
-
-            
-            
+        elif milk <= 100:
+            mMilk = "you get the rare banana milk"
+            await ctx.send(f'{ctx.author.name} {mMilk}')
+                 
     #black jack command?
     
 bot = Bot()

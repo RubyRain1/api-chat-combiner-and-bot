a099938459ad330e6de97b12a146d2360d52144b
HurricaneHolden <100702336+RubyRain1@users.noreply.github.com>
2024-04-03 20:21:22 -0400

    terminal UI+ logic
    this is just giving the user experience for the terminal version along with logic as the program begins

--- main.py
+++ main.py
@@ -2,94 +2,123 @@ from twitchio.ext import commands
 import config
 import random
 
+
+# this is the introduction of a terminal based ui incase I cannot implement a working GUI
+print(((((""""
+  _______       _ _       _            _           _                                   _            
+ |__   __|     (_) |     | |          | |         | |                                 | |           
+    | |_      ___| |_ ___| |__     ___| |__   __ _| |_    ___ ___  _ ____   _____ _ __| |_ ___ _ __ 
+    | \ \ /\ / / | __/ __| '_ \   / __| '_ \ / _` | __|  / __/ _ \| '_ \ \ / / _ \ '__| __/ _ \ '__|
+    | |\ V  V /| | || (__| | | | | (__| | | | (_| | |_  | (_| (_) | | | \ V /  __/ |  | ||  __/ |   
+    |_| \_/\_/ |_|\__\___|_| |_|  \___|_| |_|\__,_|\__|  \___\___/|_| |_|\_/ \___|_|   \__\___|_|   
+
+
+   """"")))))  
+
+print("this app is used to convert up to 10 chats into one by inputting channel names. This is helpful for any twitch streamer"
+"looking to have a cleaner chat experience during collabs! MUST HAVE COLLAB STREAMS OPEN " 
+"(support your fellow streamers even in a collab).")
+print(" ") #this just adds a space
+start = input("would you like to begin using the program? (Y/N)")
+print(" ")
+match start:
+    case "no":
+        print("thank you for taking the time to check out my chat maker it means the world to me :D - Ruby Rain <3")
+
+
+    case "yes":
 #twitch username input
-val = input("Enter your channel name: ")
+        val = input("Enter your channel name: ")
 
-class Bot(commands.Bot):
+        class Bot(commands.Bot):
     
     
     
-    def __init__(self):
+            def __init__(self):
 
-        #chat connection
-        super().__init__(token=config.api_key, prefix='?', initial_channels=[val])
+            #chat connection
+                super().__init__(token=config.api_key, prefix='?', initial_channels=[val])
         
-    async def event_message(self, messagae):
+        async def event_message(self, messagae):
 
-        print(f'Logged in as | {self.nick}')
-        print(f'user id is | {self.user_id}')
+            print(f'Logged in as | {self.nick}')
+            print(f'user id is | {self.user_id}')
 
-    #chat combiner code
-    async def event_message(self, message):
+        #chat combiner code
+        async def event_message(self, message):
 
-        if message.echo:
-            return
+            if message.echo:
+                return
         
-        rAuthor = message.author.display_name
-        rContent = message.content
+            rAuthor = message.author.display_name
+            rContent = message.content
 
-        print(f'{rAuthor}: {rContent}')
+            print(" ")
+            print(f'{rAuthor}: {rContent}')
 
-        await self.handle_commands(message)
+            await self.handle_commands(message)
 
-    # hello command
-    @commands.command()
-    async def hello(self, ctx: commands.Context):
-        await ctx.send(f'Hello {ctx.author.name}')
+        # hello command
+        @commands.command()
+        async def hello(self, ctx: commands.Context):
+            await ctx.send(f'Hello {ctx.author.name}')
 
-    #cookie command
-    @commands.command()
-    async def cookie(self, ctx: commands.Context):
-        await ctx.send(f'{ctx.author.name} gets a cookie!!!')
+        #cookie command
+        @commands.command()
+        async def cookie(self, ctx: commands.Context):
+            await ctx.send(f'{ctx.author.name} gets a cookie!!!')
 
-    #8ball command
-    @commands.command()
-    async def fortune(self, ctx: commands.Context):
-        fortune = random.randint(1,100) #this gets a random number 1-100 in order for logic to run.
-        if fortune <= 25:
-            await ctx.send(f'{ctx.author.name} yes I assume so.')
-        elif fortune <= 50:
-            await ctx.send(f'{ctx.author.name} no this is unlikely.')
-        elif fortune <= 75: 
-            await ctx.send(f'{ctx.author.name} there is but a chance.')
-        elif fortune <= 100: 
-            await ctx.send(f'{ctx.author.name} perchance :skull:')
+        #8ball command
+        @commands.command()
+        async def fortune(self, ctx: commands.Context):
+            fortune = random.randint(1,100) #this gets a random number 1-100 in order for logic to run.
+            if fortune <= 25:
+                await ctx.send(f'{ctx.author.name} yes I assume so.')
+            elif fortune <= 50:
+                await ctx.send(f'{ctx.author.name} no this is unlikely.')
+            elif fortune <= 75: 
+                await ctx.send(f'{ctx.author.name} there is but a chance.')
+            elif fortune <= 100: 
+                await ctx.send(f'{ctx.author.name} perchance :skull:')
     
 
-    #milk gamble command
-    @commands.command()
-    async def milk(self, ctx: commands.Context):
-        #this is going to be the random logic for the command.
-        milk = round(random.uniform(1,100), 2) #this gets a random float number from 1-100 and stops it at 2 dec points.
-        if milk <= 33.33:
-             mMilk = "you regular milk :3"
-             await ctx.send(f'{ctx.author.name} {mMilk}')
-        elif milk <= 66.66:
-             mMilk = "you get the kinda mid strawberry milk if we are really being honest here."
-             await ctx.send(f'{ctx.author.name} {mMilk}')
-        elif milk <= 99.99:
-            mMilk = "you get the all fated choccy milk."
-            await ctx.send(f'{ctx.author.name} {mMilk}')
-        elif milk <= 100:
-            mMilk = "you get the rare banana milk"
-            await ctx.send(f'{ctx.author.name} {mMilk}')
+         #milk gamble command
+        @commands.command()
+        async def milk(self, ctx: commands.Context):
+            #this is going to be the random logic for the command.
+            milk = round(random.uniform(1,100), 2) #this gets a random float number from 1-100 and stops it at 2 dec points.
+            if milk <= 33.33:
+                mMilk = "you regular milk :3"
+                await ctx.send(f'{ctx.author.name} {mMilk}')
+            elif milk <= 66.66:
+                mMilk = "you get the kinda mid strawberry milk if we are really being honest here."
+                await ctx.send(f'{ctx.author.name} {mMilk}')
+            elif milk <= 99.99:
+                mMilk = "you get the all fated choccy milk."
+                await ctx.send(f'{ctx.author.name} {mMilk}')
+            elif milk <= 100:
+                mMilk = "you get the rare banana milk"
+                await ctx.send(f'{ctx.author.name} {mMilk}')
                  
-    #black jack command?
-    @commands.command()
-    async def BJ(self, ctx: commands.Context):
-        dCard = random.randint(17,21)
-        pCard1 = random.randint(1,11)
-        pCard2 = random.randint(1,11)
-        pTotal = pCard1 + pCard2
+        #black jack command?
+        @commands.command()
+        async def BJ(self, ctx: commands.Context):
+            dCard = random.randint(17,21)
+            pCard1 = random.randint(1,11)
+            pCard2 = random.randint(1,11)
+            pTotal = pCard1 + pCard2
         
-        if dCard < pTotal:
-            await ctx.send(f'the dealer drew {dCard}, {ctx.author.name} pulled {pTotal} YOU WINNNNN')
-        else:
-            await ctx.send(f'the dealer drew {dCard}, {ctx.author.name} pulled {pTotal} YOU LOSE WOMP WOMP')
+            if pTotal > 21:
+                await ctx.send(f'the dealer drew {dCard}, {ctx.author.name} pulled {pTotal} YOU WINNNNN')
+            elif dCard < pTotal:
+                await ctx.send(f'the dealer drew {dCard}, {ctx.author.name} pulled {pTotal} YOU WINNNNN')
+            else:
+                await ctx.send(f'the dealer drew {dCard}, {ctx.author.name} pulled {pTotal} YOU LOSE WOMP WOMP')
       
         
 
 
 
-bot = Bot()
-bot.run()
+        bot = Bot()
+        bot.run()
+
